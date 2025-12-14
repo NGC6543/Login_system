7b52ba35184be65f410c2d7a4e10a48a0ef8b95b
Dmitry <nikel0967@gmail.com>
2025-12-14 14:24:25 +0500

    Fix readme

--- api/permissions.py
+++ api/permissions.py
@@ -26,7 +26,7 @@ class IsAdminOrStaff(permissions.BasePermission):
 
 
 class HasAccessByRole(permissions.BasePermission):
-    
+
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
